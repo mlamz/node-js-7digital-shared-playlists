7666ce860e4bd88abc52294b6f52571ed01759d5
Michael Lam <treadsafely@gmail.com>
2012-04-19 11:49:35 +0100

    add styling and track search form layout

--- public/chat.js
+++ public/chat.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
 	now.name = prompt("Whats your name?", "");
+	$("#welcome-message").text("Welcome, " + now.name);
 	
 	now.receiveMessage = function(name, message){
-	    $("#messages").append("<br />" + name + " : " + message);
+	    $("#messages").prepend("<br />" + name + " : " + message);
 	}
 	$("#send-button").click(function() {
 		
 	});
 
-	$("#chat-form").submit(function(){
+	$("#chat-form").submit(function(e){
+		e.preventDefault();
 		now.distributeMessage($("#text-input").val());
 		$("#text-input").val('');
-		return false;
 	})
 });
 
