58654e7acbe2f8ca63cdd4ce8e2f333292f08e29
Mike <mike@ubuntu.(none)>
2012-04-21 10:11:17 +0100

    now-js functionality to share the playlist

--- public/track-search.js
+++ public/track-search.js
@@ -3,7 +3,12 @@ $(document).ready(function(){
 
   now.receiveNewPlaylistTrack = function(name, track){
       console.log("receiving track...");
-      $("#sortable").append(track);
+      console.log(track);
+
+
+      var trackHtml = buildSearchResultHtml(track.image, track.artistName, track.trackName, false, false, track.trackId, track.addedBy);
+      console.log(trackHtml);
+      $("#sortable").append(trackHtml);
   }
 
 	$("#track-search").submit(function(e){
@@ -26,19 +31,31 @@ $(document).ready(function(){
 	})
 
   function bindSearchResultTriggers(){
-    var trackIdAdded;
+    var trackIdAdded, trackToPublish;
 
-      $("#searchResults li.search-result").click(function() {
-      trackIdAdded = $(this).attr("data-trackid");
+    $("#searchResults li.search-result").click(function() {
+      var searchResult = $(this);
+      trackIdAdded = searchResult.attr("data-trackid");
       
+
+
       if (trackIdAdded !== undefined && playlistTrackIds.indexOf(trackIdAdded) === -1){
         console.log("track added", trackIdAdded);
         playlistTrackIds.push(trackIdAdded);
-        $('#sortable').append(this);
+       // $('#sortable').append(this);
+
+        trackToPublish = {
+          trackId: trackIdAdded,
+          trackName: searchResult.attr("data-trackname"),
+          artistName: searchResult.attr("data-artistname"),
+          image: searchResult.attr("data-image"),
+          addedBy: now.name
+        }
         //distributeNewPlaylistTrack
-        //now.distributeNewPlaylistTrack(JSON.parse(this));
+        now.distributeNewPlaylistTrack(trackToPublish);
       }
       console.log("tracks in playlist", playlistTrackIds);
+      console.log("track to distribute", trackToPublish);
 
       playNextTrack();
       
@@ -70,18 +87,21 @@ $(document).ready(function(){
     }
   }
 
-  function buildSearchResultHtml(image, artist, track, isLast, isRepeatedTrack, trackId){
+  function buildSearchResultHtml(image, artist, track, isLast, isRepeatedTrack, trackId, addedBy){
     var html = "";
     artist = artist.length > 12 ? artist.substr(0, 12) + "..." : artist;
     track = track.length > 25 ? track.substr(0, 25) + "..." : track;
+    addedBy = (addedBy !== undefined) ? "Added by " + addedBy : "";
     if(!isRepeatedTrack){
-      html = "<li class='search-result ui-widget-content playlist-spot-container' data-trackid='"+trackId+"'>"
+      html = "<li class='search-result ui-widget-content playlist-spot-container' data-trackid='"+trackId+"' data-image='"+ image +"' data-artistname='"+ artist +"' data-trackname='"+ track +"'>"
       +"<div class='column first'><img src='" + image + "' /></div>"
       +"<div class='column'><span class='small' style='font-size:10px'>"
       + artist
       + " - "
       + track
-      + "</span></li>";
+      + "</span>"
+      + "<p>" + addedBy + "</p>"
+      "</li>";
     }
     
     if (isLast){
