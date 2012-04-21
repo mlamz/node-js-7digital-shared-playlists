9805d2243e72f4a4777d7ecaaec9528684e53b24
Mike <mike@ubuntu.(none)>
2012-04-21 08:34:54 +0100

    removed drag and drop as the UI was not predictable or responsive enough. Fixed play next track functionality.

--- public/track-search.js
+++ public/track-search.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var currentReleaseId, currentTrackId;
+	var currentTrackId, playlistTrackIds = [], trackCurrentlyPlaying = false;
 
 	$("#track-search").submit(function(e){
 		e.preventDefault();
@@ -12,102 +12,85 @@ $(document).ready(function(){
      			var track = searchResult.track,
                 isLast = (index + 1 == data.response.searchResults.searchResult.length),
                 isRepeatedTrack = $("#searchResults").text().indexOf(track.title) !== -1;
-   				$("#searchResults").append(buildSearchResultHtml(track.release.image, track.artist.name, track.title, isLast, isRepeatedTrack, track.release.id, track.id));
+   				$("#searchResults").append(buildSearchResultHtml(track.release.image, track.artist.name, track.title, isLast, isRepeatedTrack, track.id));
           
         })
 
-        	$(".search-result").draggable({ 
-            revert: "invalid", 
-            snap: "#shared-playlist", 
-            snapMode: "inner", 
-            snapTolerance: "100", 
-            connectToSortable: "#sortable",
-            helper: "clone",
-          });
-
-          $("playlist-spot-container").draggable({ 
-            snap: true,
-          });
-
-        	$("#shared-playlist").droppable({
-      			activeClass: "shared-playlist-hover",
-      			hoverClass: "shared-playlist-active",
-      			drop: function( event, ui ) {
-
-              reorderPlayList();
-              var trackid = getTrackId();
-              playTrack(trackid);
-              
-      			}
-		      });
+        bindSearchResultTriggers();
+        
+          
+        
 
           $( "#sortable" ).sortable({  
             revert: true, 
-            cancel: ".ui-state-disabled", 
-            //placeholder: "placeholder-highlight"  
+            cancel: ".ui-state-disabled",
           });
 
           $( "ul, li" ).disableSelection();
-   		
-   		function buildSearchResultHtml(image, artist, track, isLast, isRepeatedTrack, releaseId, trackId){
-   			var html = "";
-
-        if(!isRepeatedTrack){
-          html = "<li class='search-result ui-widget-content playlist-spot-container' data-releaseid='"+releaseId+"' data-trackid='"+trackId+"'>"
-          +"<div class='column first'><img src='" + image + "' /></div>"
-          +"<div class='column'><span class='small' style='font-size:10px'>"
-          + artist 
-          + " - "
-          + track 
-          + "</span></li>";
-        }
-        if (isLast){
-          html += "</ul>";
-        }
-        return html;
-   		}
-
-      function reorderPlayList(){
-          $("li.playlist-spot").remove();
-          $("ul#sortable").append('<li class="playlist-spot ui-state-disabled"></li>');
-          $("ul#sortable").append('<li class="playlist-spot ui-state-disabled"></li>');
-          $("ul#sortable").append('<li class="playlist-spot ui-state-disabled"></li>');
-          $("ul#sortable").append('<li class="playlist-spot ui-state-disabled"></li>');
-      };
-      var trackCurrentlyPlaying;
-      function playTrack(trackid){
-        
-        if (!trackCurrentlyPlaying){
-
-          trackCurrentlyPlaying = true;
-          var stream = "http://previews.7digital.com/clips/34/"+trackid+".clip.mp3";
-          $('#audio-stream').attr('src', stream);
-          var a = audiojs.createAll({
-            trackEnded: function() {
-              console.log("track ended, removing trackid " + trackid);
-              $("#sortable li.search-result[data-trackid="+ trackid +"]").remove();
-                //trackCurrentlyPlaying = false;
-                console.log("removed...")
-                var trackid = playNextTrack();
-                //if(trackid){
-                //trackCurrentlyPlaying = true;
-                }
-            }
-          );
-         // $("#music-player").hide();  
-        }
-      }
-           
-        
-      
-
-      function getTrackId(){
-        var trackid = $('#sortable').find('li.search-result[data-trackid]').attr("data-trackid");
-        console.log(trackid);
-        return trackid;
-      }
-
  		})
 	})
+
+  function bindSearchResultTriggers(){
+    var trackIdAdded;
+
+      $("#searchResults li.search-result").click(function() {
+      trackIdAdded = $(this).attr("data-trackid");
+      
+      if (trackIdAdded !== undefined && playlistTrackIds.indexOf(trackIdAdded) === -1){
+        console.log("track added", trackIdAdded);
+        playlistTrackIds.push(trackIdAdded);
+        $('#sortable').append(this);
+      }
+      console.log("tracks in playlist", playlistTrackIds);
+
+      playNextTrack();
+      
+    })
+  }
+  function playNextTrack(){
+    if (playlistTrackIds.length > 0 && !trackCurrentlyPlaying){
+          playTrack(playlistTrackIds[0]);
+      }
+  }
+
+  function playTrack(trackid){
+    if (!trackCurrentlyPlaying){
+      console.log("playing track id ", trackid);
+      trackCurrentlyPlaying = true;
+      var stream = "http://previews.7digital.com/clips/34/"+trackid+".clip.mp3";
+      $('#audio-stream').attr('src', stream);
+      var a = audiojs.createAll({
+        trackEnded: function() {
+          trackCurrentlyPlaying = false;
+          console.log("track ended, removing trackid " + trackid);
+          $("#sortable li.search-result[data-trackid="+ trackid +"]").remove();
+          playlistTrackIds.shift();
+          console.log("removed...", trackid);
+          playNextTrack();
+        }
+     }); 
+    }
+  }
+
+  function buildSearchResultHtml(image, artist, track, isLast, isRepeatedTrack, trackId){
+    var html = "";
+
+    if(!isRepeatedTrack){
+      html = "<li class='search-result ui-widget-content playlist-spot-container' data-trackid='"+trackId+"'>"
+      +"<div class='column first'><img src='" + image + "' /></div>"
+      +"<div class='column'><span class='small' style='font-size:10px'>"
+      + artist 
+      + " - "
+      + track 
+      + "</span></li>";
+    }
+    
+    if (isLast){
+      html += "</ul>";
+    }
+    
+    return html;
+  }
+
 })
 
